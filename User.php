30a8b1c52d2a19ca8c2cd0458aa43f5ebb53880f
camara_aicha <aicha.ca@outlook.fr>
2024-08-26 14:21:01 +0200

    Ajout de la fonction __construct qui sert  pour la connexion à la base de données et les attributs de l'utilisateur

--- User.php
+++ User.php
@@ -1,13 +1,23 @@
+<?php
+
+require_once'config.php';
+
 class User{
 
-    private $id
-    public $login
-    public $email
-    public $firstname
-    public $lastname
+    private $id;
+    public $login;
+    public $email;
+    public $firstname;
+    public $lastname;
+    private $conn;
 
-
-
-
-
-}
+    public function __construct($conn,$login='',$email='',$firstname='',$lastname=''){
+           
+        $this->conn=$conn;
+        $this->login=$login;
+        $this->email=$email;
+        $this->firstname=$firstname;
+        $this->lastname=$lastname;
+    }
+    p 
+?>
